a00b020e62277af122170a67a3205785f788e383
TibiBejan <tiberiu.bejan98@gmail.com>
2022-06-04 12:49:42 +0300

    prepared production build v2

--- config/db.config.js
+++ config/db.config.js
@@ -24,12 +24,12 @@ module.exports = {
     }
   },
   production: {
-    username: process.env.PROD_DB_USERNAME,
-    password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_NAME,
-    host: process.env.PROD_DB_HOSTNAME,
-    port: process.env.PROD_DB_PORT,
-    dialect: 'mysql',
+    // username: process.env.PROD_DB_USERNAME,
+    // password: process.env.PROD_DB_PASSWORD,
+    // database: process.env.PROD_DB_NAME,
+    // host: process.env.PROD_DB_HOSTNAME,
+    // port: process.env.PROD_DB_PORT,
+    // dialect: 'mysql',
     // dialectOptions: {
     //   bigNumberStrings: true,
     //   ssl: {
