13fef1aa3d519b5f80b9bc9fbfa086be85deadbd
TibiBejan <tiberiu.bejan98@gmail.com>
2022-06-04 13:07:15 +0300

    production build - fixed sequelize dialect missing

--- config/db.config.js
+++ config/db.config.js
@@ -24,17 +24,17 @@ module.exports = {
     }
   },
   production: {
-    // username: process.env.PROD_DB_USERNAME,
-    // password: process.env.PROD_DB_PASSWORD,
-    // database: process.env.PROD_DB_NAME,
-    // host: process.env.PROD_DB_HOSTNAME,
-    // port: process.env.PROD_DB_PORT,
-    // dialect: 'mysql',
-    // dialectOptions: {
-    //   bigNumberStrings: true,
-    //   ssl: {
-    //     ca: fs.readFileSync(__dirname + '/mysql-ca-main.crt')
-    //   }
-    // }
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOSTNAME,
+    port: process.env.PROD_DB_PORT,
+    dialect: 'mysql',
+    dialectOptions: {
+      bigNumberStrings: true,
+      ssl: {
+        ca: fs.readFileSync(__dirname + '/mysql-ca-main.crt')
+      }
+    }
   }
 };
