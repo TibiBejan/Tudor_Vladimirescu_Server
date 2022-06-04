ca095a20edb6b6ee43ec6e027acf5db11e33be69
TibiBejan <tiberiu.bejan98@gmail.com>
2022-06-04 13:33:32 +0300

    production build - sequelized production db set vars

--- config/db.config.js
+++ config/db.config.js
@@ -24,17 +24,14 @@ module.exports = {
     }
   },
   production: {
-    username: process.env.PROD_DB_USERNAME,
-    password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_NAME,
-    host: process.env.PROD_DB_HOSTNAME,
-    port: process.env.PROD_DB_PORT,
-    dialect: 'mysql'
-    // dialectOptions: {
-    //   bigNumberStrings: true,
-    //   ssl: {
-    //     ca: fs.readFileSync(__dirname + '/mysql-ca-main.crt')
-    //   }
-    // }
+    use_env_variable: 'JAWSDB_URL',
+    dialect: 'mysql',
+    dialectOptions: {
+      bigNumberStrings: true,
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
