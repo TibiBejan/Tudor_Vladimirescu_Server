10bc2c7f17a8bd23d3159bf2e60e89c5856fde0c
TibiBejan <tiberiu.bejan98@gmail.com>
2022-06-09 22:31:44 +0300

    Added cors middleware

--- config/express.config.js
+++ config/express.config.js
@@ -13,16 +13,25 @@ import routes from '../routes/index.js';
 const app = express()
 
 // =================== Global Middlewares =================== //
-var corsOptions = {
-    'Access-Control-Allow-Origin': '*',
-}
+// var corsOptions = {
+//     'Access-Control-Allow-Origin': '*',
+// }
   
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('combined'));
+
+app.use(cors(), function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", ["http://localhost:3000", "https://tudor-vladimirescu.netlify.app"]); // update to match the domain you will make the request from
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 
 // // This middleware takes care of the origin when the origin is undefined. Origin is undefined when request is local
 // app.use((req, _, next) => {
