94b561f2a2cb7f64e69d6a0085757824591900aa
TibiBejan <tiberiu.bejan98@gmail.com>
2022-06-09 22:24:19 +0300

    Use default cors on express app and commented undefined origin middleware

--- config/express.config.js
+++ config/express.config.js
@@ -19,18 +19,18 @@ var corsOptions = {
     'Access-Control-Allow-Origin': '*',
 }
   
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('combined'));
 
-// This middleware takes care of the origin when the origin is undefined. Origin is undefined when request is local
-app.use((req, _, next) => {
-    req.headers.origin = req.headers.origin || req.headers.host;
-    next();
-});
+// // This middleware takes care of the origin when the origin is undefined. Origin is undefined when request is local
+// app.use((req, _, next) => {
+//     req.headers.origin = req.headers.origin || req.headers.host;
+//     next();
+// });
 
 // =================== Routes =================== //
 app.get('/api/v1', (req, res) => {
