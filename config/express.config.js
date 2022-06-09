a765bf5ceea2540fe62710837a1bc5d85bf5b0fc
TibiBejan <tiberiu.bejan98@gmail.com>
2022-06-09 16:44:44 +0300

    Added localhost on header allow origin

--- config/express.config.js
+++ config/express.config.js
@@ -15,7 +15,7 @@ const app = express()
 // =================== Global Middlewares =================== //
 var corsOptions = {
     'Access-Control-Allow-Credentials': true,
-    'Access-Control-Allow-Origin': 'https://tudor-vladimirescu.netlify.app',
+    'Access-Control-Allow-Origin': ['https://tudor-vladimirescu.netlify.app', 'http://localhost'],
 }
   
 app.use(cors(corsOptions));
