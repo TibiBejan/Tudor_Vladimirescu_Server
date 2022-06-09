e53873d4fe47116c4caeabab829cf844f81e5f46
TibiBejan <tiberiu.bejan98@gmail.com>
2022-06-09 22:43:27 +0300

    Added cors middleware with single allowed origin

--- config/express.config.js
+++ config/express.config.js
@@ -13,25 +13,23 @@ import routes from '../routes/index.js';
 const app = express()
 
 // =================== Global Middlewares =================== //
-// var corsOptions = {
-//     'Access-Control-Allow-Origin': '*',
-// }
-  
-// app.use(cors());
+var whitelist = ['https://tudor-vladimirescu.netlify.app/', 'http://localhost:3000']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('combined'));
-
-app.use(cors(), function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", ["https://tudor-vladimirescu.netlify.app"]); // update to match the domain you will make the request from
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-});
 
 // // This middleware takes care of the origin when the origin is undefined. Origin is undefined when request is local
 // app.use((req, _, next) => {
