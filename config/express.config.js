07a024dd551b62956349502c4458a5ddbf74aacb
TibiBejan <tiberiu.bejan98@gmail.com>
2022-06-09 22:38:42 +0300

    Added cors middleware with single allowed origin

--- config/express.config.js
+++ config/express.config.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan('combined'));
 
 app.use(cors(), function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", ["http://localhost:3000", "https://tudor-vladimirescu.netlify.app"]); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", ["https://tudor-vladimirescu.netlify.app"]); // update to match the domain you will make the request from
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
