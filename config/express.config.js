5b69372b606a371cd2d0bee032910dee78ed406b
TibiBejan <tiberiu.bejan98@gmail.com>
2022-06-09 22:49:13 +0300

    Added cors middleware with single allowed origin

--- config/express.config.js
+++ config/express.config.js
@@ -13,29 +13,36 @@ import routes from '../routes/index.js';
 const app = express()
 
 // =================== Global Middlewares =================== //
-var whitelist = ['https://tudor-vladimirescu.netlify.app', 'http://localhost:3000', '*']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+// var whitelist = ['https://tudor-vladimirescu.netlify.app', 'http://localhost:3000', '*']
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('combined'));
 
-// // This middleware takes care of the origin when the origin is undefined. Origin is undefined when request is local
-// app.use((req, _, next) => {
-//     req.headers.origin = req.headers.origin || req.headers.host;
-//     next();
-// });
+// Cors
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+// This middleware takes care of the origin when the origin is undefined. Origin is undefined when request is local
+app.use((req, _, next) => {
+    req.headers.origin = req.headers.origin || req.headers.host;
+    next();
+});
 
 // =================== Routes =================== //
 app.get('/api/v1', (req, res) => {
