5f55e24e38e26e4df35eb5df4bf9908e34944d83
TibiBejan <tiberiu.bejan98@gmail.com>
2022-06-11 22:03:42 +0300

    added middleware for cors

--- config/express.config.js
+++ config/express.config.js
@@ -24,6 +24,15 @@ app.use(morgan('combined'));
 app.all('/', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+});
+
+app.use((req, res, next) => {
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+      return res.status(200).json({});
+    }
     next();
 });
 
