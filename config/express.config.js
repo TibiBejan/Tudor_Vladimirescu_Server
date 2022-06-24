6997bfd24ea93330961042aa2d9fcd3333da5593
TibiBejan <tiberiu.bejan98@gmail.com>
2022-06-24 08:11:09 +0300

    try fix for cors bug

--- config/express.config.js
+++ config/express.config.js
@@ -15,16 +15,17 @@ const app = express()
 // =================== Global Middlewares =================== //
 const whitelist = ["http://localhost:3000", "https://tudor-vladimirescu.netlify.app"]
 app.use(cors({
-    origin: function (origin, callback) {
-        if(!origin) return callback(null, true);    
+    // origin: function (origin, callback) {
+    //     if(!origin) return callback(null, true);    
         
-        if(whitelist.indexOf(origin) === -1){
-            var msg = 'The CORS policy for this site does not ' +
-                      'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
+    //     if(whitelist.indexOf(origin) === -1){
+    //         var msg = 'The CORS policy for this site does not ' +
+    //                   'allow access from the specified Origin.';
+    //         return callback(new Error(msg), false);
+    //     }
+    //     return callback(null, true);
+    // },
+    origin: ["http://localhost:3000", "https://tudor-vladimirescu.netlify.app"],
     optionsSuccessStatus: 200,
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
